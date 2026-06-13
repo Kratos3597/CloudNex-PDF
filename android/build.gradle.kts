900351d34fc87fc4a9627a4476f26cadc5b82cf1
Cirrus CI <support@cirruslabs.org>
2026-06-13 07:49:28 +0000

    Final cleanup and fix build errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,20 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("com.android.library") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+// This block forces all plugins to agree on the SDK version
+subprojects {
+    afterEvaluate {
+        extensions.findByName("android")?.let { android ->
+            val androidExt = android as com.android.build.gradle.BaseExtension
+            androidExt.compileSdkVersion(36) // Force consistency
+            androidExt.defaultConfig {
+                targetSdkVersion(36)
+            }
+        }
     }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
