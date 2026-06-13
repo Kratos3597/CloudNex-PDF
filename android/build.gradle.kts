914af1b6da853e47bed90a8f66b4a4e5a78aa590
Cirrus CI <support@cirruslabs.org>
2026-06-13 08:12:07 +0000

    Final cleanup and fix build errors

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file
 plugins {
     // Keep your environment's versions here (Do not change)
-    id("com.android.application") version "9.0.1" apply false
-    id("com.android.library") version "9.0.1" apply false
+    id("com.android.application") apply false
+    id("com.android.library")apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
